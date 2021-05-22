5e9653fc1ff49806449b5b82e9cb9a9940dc1e05
Sarah Hinton <smcfarland2@gmail.com>
2021-05-22 15:24:48 +1000

    adding database

---
+++ db/tracker_db.sql
@@ -0,0 +1,31 @@
+DROP DATABASE if exists tracker_db;
+
+CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department (
+ id INT AUTO_INCREMENT,
+ PRIMARY KEY(id),
+ name VARCHAR(30) 
+);
+
+CREATE TABLE role (
+ id INT AUTO_INCREMENT,
+ PRIMARY KEY(id),
+ departmentID INT,
+ title VARCHAR(30),
+ salary DECIMAL,
+ FOREIGN KEY (departmentID) REFERENCES department(id)
+ );
+ 
+ CREATE TABLE employee (
+ id INT AUTO_INCREMENT NOT NULL,
+ PRIMARY KEY(id),
+ roleID INT,
+ first_name VARCHAR(30),
+ last_name VARCHAR(30),
+ FOREIGN KEY (roleID) REFERENCES role(id)
+ ); 
+ 
+ 
