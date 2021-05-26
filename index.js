3564184f4e2f033ae70f94f5c4e384f6ffb905d6
Sarah Hinton <smcfarland2@gmail.com>
2021-05-26 21:26:30 +1000

    adding more start menu options for creating data

--- index.js
+++ index.js
@@ -7,27 +7,40 @@ const Role = require('./models/Role');
 
 var inquirer = require('inquirer');
 
-const begin
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-        type: 'list',
-        name : 'start',
-        message: 'What would you like to do?',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'View employees by department', 'Update employees']
-    }
-    
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    // switch on answers.start
-    //case
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const begin = () => {
+  inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+          type: 'list',
+          name : 'start',
+          message: 'What would you like to do?',
+          choices: ['View all departments', 'View all roles', 'View all employees', 'Add Department', 'Add Role', 'Add Employee', 'Update employees']
+      }
+      
+    ])
+    .then((answers.) => {
+      // Use user feedback for... whatever!!
+      // switch on answers.start
+      switch (answers.start) {
+        case 'View all departments':
+          dptView();
+
+        case 'View all roles':
+          roleView();
+
+        case 'View all employees':
+          employeeView();
+
+        case upda
+      } 
+      //case
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+};
