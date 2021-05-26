0f2e72814b4ca5025533b8fe62a7a3feefe9413d
Sarah Hinton <smcfarland2@gmail.com>
2021-05-26 21:01:11 +1000

    coding index.js

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const sequelize = require('./config/connection');
+const mysql = require('./config/connection');
 
 // Import model to sync table with database
-const Book = require('./models/Book');
+const Department = require('./models/Department');
+const Employee = require('./models/Employee');
+const Role = require('./models/Role');
 
+var inquirer = require('inquirer');
+
+const begin
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type: 'list',
+        name : 'start',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'View employees by department', 'Update employees']
+    }
+    
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    // switch on answers.start
+    //case
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
